f43489330020a01900ec1c66bd49dbe8eac3452a
JesperRipnas <jepperipnas@gmail.com>
2026-02-15 21:23:48 +0100

    removed legacy controller

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule as BetterAuthModule } from '@thallesp/nestjs-better-auth';
 import { auth } from './auth.js';
 import { AuthMigrationsService } from './auth.migrations.service.js';
-import { AuthController } from './auth.controller.js';
 
 @Module({
   imports: [BetterAuthModule.forRoot(auth)],
-  controllers: [AuthController],
+  controllers: [],
   providers: [AuthMigrationsService],
 })
 export class AuthModule {}
